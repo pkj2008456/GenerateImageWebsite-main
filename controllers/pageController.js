78e33afdb6d013ae6695f588651145cf948333d5
pkj2008456 <pkj2003456@gmail.com>
2024-08-23 11:54:38 +0800

    easy login system

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,18 +1,31 @@
-// controllers/pageController.js
+const fs = require('fs');
+
 exports.renderMainPage = (req, res) => {
+    console.log('req.session.userId:', req.session.userId);
     res.render('MainPage');
 };
 
 exports.renderLibrary = (req, res) => {
-    res.render('Library');
+    const userId = req.session.userId;
+    if (!userId) {
+        return res.redirect('/login'); // 如果没有登录，重定向到登录页
+    }
+    const userFolder = `path/to/user_folders/${userId}`;
+
+    // 读取用户文件夹中的图片
+    fs.readdir(userFolder, (err, files) => {
+        if (err) {
+            res.status(500).send('Error reading user images');
+            return;
+        }
+
+        const imageUrls = files.map(file => `/user_folders/${userId}/${file}`);
+        res.render('Library', { userId, imageUrls });
+    });
 };
 
 exports.renderImageGen = (req, res) => {
     res.render('ImageGen');
-};
-
-exports.renderProfile = (req, res) => {
-    res.render('Profile');
 };
 
 exports.renderSetting = (req, res) => {
@@ -21,4 +34,39 @@ exports.renderSetting = (req, res) => {
 
 exports.renderUpgradePlan = (req, res) => {
     res.render('UpgradePlan');
+};
+
+exports.renderLogin = (req, res) => {
+    res.render('login');
+};
+
+exports.handleLogin = (req, res) => {
+    console.log(req.session);
+    const userId = req.body.userId;
+    if (userId) {
+        // 将 userId 存储到 session 中
+        req.session.userId = userId;
+        res.redirect('/mainpage'); // 登录后重定向到 mainpage
+    } else {
+        res.redirect('/login');
+    }
+};
+
+exports.renderProfile = (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+        return res.redirect('/login'); // 如果没有登录，重定向到登录页
+    }
+    const userFolder = `path/to/user_folders/${userId}`;
+
+    // 读取用户文件夹中的图片
+    fs.readdir(userFolder, (err, files) => {
+        if (err) {
+            res.status(500).send('Error reading user images');
+            return;
+        }
+
+        const imageUrls = files.map(file => `/user_folders/${userId}/${file}`);
+        res.render('profile', { userId, imageUrls });
+    });
 };
